6357276389d7ad3b41b05c58fef91d56cf20dcc7
3picF4iL <haskypl1@gmail.com>
2022-05-15 18:20:03 +0200

    main: remove temporary window creation

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ from general.func import set_window_with_size
 from general.maps.map1.map1 import GameViewStart
 # from general.menu.start_menu import MainMenu
 
-#arcade.configure_logging()
 
 def main():
-    #window = set_window_with_size()
-    window = arcade.Window()
+    window = set_window_with_size()
     # main_menu = MainMenu()
     # window.show_view(main_menu)
     game = GameViewStart()
